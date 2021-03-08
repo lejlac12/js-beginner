f1d8f8767ef4230659b86b2b409d1e2f7dfa8f26
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-03-08 14:43:30 +0100

    Video 12, tipovi funkcija

---
+++ Video 12/index.js
@@ -0,0 +1,12 @@
+//izvođenje zadatka
+function greet(color, box) {
+    console.log('Hejj ' + color + ' ' + box);
+}
+
+//izracunavanje vrijednosti 
+function square(number) {
+    return number * number;
+}
+
+let number = square(5);
+console.log(number);
