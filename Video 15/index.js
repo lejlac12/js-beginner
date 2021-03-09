bada09ba78cc4bb57f1771cc7e8218664f314299
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-03-09 14:42:08 +0100

    Video 15, operatori usporedbe

---
+++ Video 15/index.js
@@ -0,0 +1,11 @@
+let x = 1; //operatori usporedbe
+
+//relacijski
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+//jednakost
+console.log(x === 1);
+console.log(x !== 1);
