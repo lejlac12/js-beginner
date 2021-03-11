c5424b5a9a394ef8c6b625362f05f61936a86691
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-03-11 10:36:32 +0100

    ispravka

--- Video 15/index.js
+++ Video 15/index.js
@@ -1,11 +1,3 @@
 let x = 1; //operatori usporedbe
 
-//relacijski
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
-
-//jednakost
-console.log(x === 1);
-console.log(x !== 1);
+console.log(x >= 2);
