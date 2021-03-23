92aa93f68884352ae791558fd8bc6e3dba65d68a
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-03-23 20:38:29 +0100

    Video 24

---
+++ Video 24/index.js
@@ -0,0 +1,25 @@
+let days = 4;
+
+switch (days) {
+
+    case 1:
+        console.log("Ponedjeljak");
+        break;
+    case 2:
+        console.log("Utorak");
+        break;
+    case 3:
+        console.log("Srijeda");
+        break;
+    case 4:
+        console.log("Cetvrtak");
+        break;
+    case 5:
+        console.log("Petak");
+        break;
+    case 6:
+        console.log("Subota");
+        break;
+    case 7:
+        console.log("Nedjelja");
+}
