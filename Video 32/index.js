c1d526bc963fce8ef0817a69d70624e556861117
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-03-24 15:04:02 +0100

    Video 32

---
+++ Video 32/index.js
@@ -0,0 +1,14 @@
+let number = max(5, 10);
+console.log(number);
+
+function max(a, b) {
+    if (a > b) return a;
+    else return b;
+}
+
+/*let number = min(5, 2);
+console.log(number);
+
+function max(a, b) {
+    return (a > b) ? a : b;
+}*/
