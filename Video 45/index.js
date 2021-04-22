98a6a948be93df5dfaa7e2fd76bba5763944ce9c
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-04-22 15:13:55 +0200

    Video 45

---
+++ Video 45/index.js
@@ -0,0 +1,19 @@
+// Factory Function
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+    };
+}
+
+const myCircle = createCircle(1);
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
